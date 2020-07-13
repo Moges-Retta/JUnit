070d9fdcb969967704a8e2267b0d6379d219ff4a
Moges <Moges.ash@gmail.com>
2020-07-13 14:49:32 +0200

    isPalindroom solved

---
+++ src/main/java/be/vdab/theorie/Palindrom.java
@@ -0,0 +1,19 @@
+package be.vdab.theorie;
+
+public class Palindrom {
+    private final String woord;
+
+    public Palindrom(String woord) {
+        this.woord = woord;
+    }
+    public boolean isPalindrom(){
+        StringBuilder str = new StringBuilder(woord);
+        var ot = woord.toLowerCase();
+        var rt = str.reverse().toString().toLowerCase();
+        return ot.equals(rt);
+    }
+    @Override
+    public String toString() {
+        return woord;
+    }
+}
---
+++ src/test/java/PalindromTest.java
@@ -0,0 +1,20 @@
+import be.vdab.theorie.Palindrom;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PalindromTest {
+    @Test
+    @DisplayName("Test of een woord in reverse is tegelijk als de woord ")
+    void isEenWoordPalindrom(){
+        var palindrom = new Palindrom("lepel");
+        assertThat(palindrom.isPalindrom()).isTrue();
+    }
+    @Test
+    @DisplayName("Test of een woord in reverse is tegelijk als de woord ")
+    void isEenWoordNietPalindrom(){
+        var palindrom = new Palindrom("Moges");
+        assertThat(palindrom.isPalindrom()).isFalse();
+    }
+}
