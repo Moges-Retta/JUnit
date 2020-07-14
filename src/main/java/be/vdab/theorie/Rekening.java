6aa93f4f979f9226e64577484362b8ed3c8da1b7
Moges <Moges.ash@gmail.com>
2020-07-14 13:53:15 +0200

    Grenswaarde

--- src/main/java/be/vdab/theorie/Rekening.java
+++ src/main/java/be/vdab/theorie/Rekening.java
@@ -3,19 +3,23 @@ package be.vdab.theorie;
 import util.OnvoldoendeSaldoException;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Rekening {
     private BigDecimal saldo = BigDecimal.ZERO;
-
+    private final List<BigDecimal> stortingen = new ArrayList<>();
     public void stort(BigDecimal bedrag) {
         if (bedrag.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Bedrag moet positief zijn");
         }
-        saldo = saldo.add(bedrag);
+        stortingen.add(bedrag);
     }
 
     public BigDecimal getSaldo() {
-        return saldo;
+        return stortingen.stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public void haalAf(BigDecimal bedrag){
@@ -24,4 +28,11 @@ public class Rekening {
         }
         saldo=saldo.subtract(bedrag);
     }
+    public List<BigDecimal> getStortingen() {
+        return stortingen;
+    }
+    public List<BigDecimal> getStortingenGesorteerd() {
+        return stortingen.stream().sorted().collect(Collectors.toList());
+    }
+
 }
--- src/test/java/be/vdab/theorie/RekeningTest.java
+++ src/test/java/be/vdab/theorie/RekeningTest.java
@@ -10,42 +10,80 @@ import static org.assertj.core.api.Assertions.*;
 
 class RekeningTest {
     private Rekening rekening;
+
     @BeforeEach
-    void beforeEach(){
-         rekening = new Rekening();
+    void beforeEach() {
+        rekening = new Rekening();
     }
+
     @Test
     void hetSaldoVanEenNieuweRekeningIs0€() {
         assertThat(rekening.getSaldo()).isZero();
     }
+
     @Test
     void nadatJe10€StortIsHetSaldo10€() {
         rekening.stort(BigDecimal.TEN);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("10");
     }
+
     @Test
     void nadatJe10€En1€StortIsHetSaldo11€() {
         rekening.stort(BigDecimal.TEN);
         rekening.stort(BigDecimal.ONE);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("11");
     }
+
     @Test
     void hetGestorteBedragMagNietNulZijn() {
         assertThatIllegalArgumentException().isThrownBy(
-        () -> rekening.stort(BigDecimal.ZERO));
+                () -> rekening.stort(BigDecimal.ZERO));
     }
+
     @Test
     void hetGestortBedragMagNietNegatiefZijn() {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> rekening.stort(BigDecimal.valueOf(-1)));
     }
+
     @Test
     void stortMetNullKanNiet() {
         assertThatNullPointerException().isThrownBy(() -> rekening.stort(null));
     }
+
     @Test
     void saldoTeWeinig() {
         assertThatExceptionOfType(OnvoldoendeSaldoException.class)
                 .isThrownBy(() -> rekening.haalAf(BigDecimal.TEN));
+    }
+
+    private static final BigDecimal TWEE = BigDecimal.valueOf(2);
+
+    @Test
+    void eenNieuweRekeningHeeftGeenStortingen() {
+        assertThat(rekening.getStortingen()).isEmpty();
+    }
+
+    @Test
+    void nadatJe10€StortIsErEenStortingVan10€() {
+        rekening.stort(BigDecimal.TEN);
+        assertThat(rekening.getStortingen()).containsOnly(BigDecimal.TEN);
+    }
+
+    @Test
+    void nadatJe10€En1€StortZijnDeStortingen10€En1€() {
+        rekening.stort(BigDecimal.TEN);
+        rekening.stort(BigDecimal.ONE);
+        assertThat(rekening.getStortingen())
+                .containsExactly(BigDecimal.TEN, BigDecimal.ONE);
+    }
+
+    @Test
+    void nadatJe10€En1€En2€StortZijnDeStortingenGesorteerd1€En2€En10€() {
+        rekening.stort(BigDecimal.TEN);
+        rekening.stort(BigDecimal.ONE);
+        rekening.stort(TWEE);
+        assertThat(rekening.getStortingenGesorteerd()).isSorted()
+                .containsExactly(BigDecimal.ONE, TWEE, BigDecimal.TEN);
     }
 }
