06f89ab64d95dc766085973e7276aac8a3a19486
Moges <Moges.ash@gmail.com>
2020-07-14 10:38:37 +0200

    Veiling BeforeEach method

--- src/test/java/be/vdab/theorie/VeilingTest.java
+++ src/test/java/be/vdab/theorie/VeilingTest.java
@@ -1,27 +1,29 @@
 package be.vdab.theorie;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VeilingTest {
-
+    private Veiling veiling;
+    @BeforeEach
+    void beforeEach(){
+        veiling = new Veiling();
+    }
     @Test
     void nogGeenBodIsBodZero() {
-        var veiling = new Veiling();
         assertThat(veiling.getHoogsteBod()).isZero();
     }
 
     @Test
     void erresteBodisHoogsteBod() {
-        var veiling = new Veiling();
         veiling.doeBod(1000);
         assertThat(veiling.getHoogsteBod()).isEqualByComparingTo(1000);
     }
 
     @Test
     void bodBedragIsHoogsteBod() {
-        var veiling = new Veiling();
         veiling.doeBod(20);
         veiling.doeBod(200);
         veiling.doeBod(2000);
